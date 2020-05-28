818b122a2fab7ecb3aca17d9d985bbf1c0992593
Kyrill <kyrill.gobber@gmail.com>
2020-05-28 20:26:48 +0200

    Added some quotes

--- js/script.js
+++ js/script.js
@@ -7,16 +7,49 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
-
+const quotes = [];
+quotes.push({
+    quote: 'Strength does not come from physical capacity. It comes from an indomitable will.',
+    source: 'Mahatma Gandhi'
+});
+quotes.push({
+    quote: 'Part of the journey is the end.',
+    source: 'Tony Stark',
+    citation: 'Avengers: Endgame',
+    year: '2019'
+});
+quotes.push({
+    quote: 'Everything That Has A Beginning Has An End.',
+    source: 'The Oracle',
+    citation: 'The Matrix: Revolutions',
+    year: '2003'
+});
+quotes.push({
+    quote: 'Denial is the most predictable of all human responses.',
+    source: 'The Architect',
+    citation: 'The Matrix: Reloaded',
+    year: '2003'
+});
+quotes.push({
+    quote: 'Look at these poor fuckers. They don\'t even know what\'s about to hit \'em. The hammer of God.',
+    source: 'Erlich Bachman',
+    citation: 'Silicon Valley',
+    year: '2014'
+});
+quotes.push({
+    quote: 'Your vision will become clear only when you look into your heart ... Who looks outside, dreams. Who looks inside, awakens.',
+    source: 'Carl Gustav Jung'
+});
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote() {
 
+}
 
 
 /***
