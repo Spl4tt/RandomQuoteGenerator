f44f9d910918b1654c62d274cc645515af3e6867
Kyrill <kyrill.gobber@gmail.com>
2020-05-30 03:25:32 +0200

    Added Additional quote object property: Tags. Small CSS adjustments for new Property.

--- js/script.js
+++ js/script.js
@@ -8,40 +8,57 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /***
+ * Tag Strings
+ */
+const moviesTag = 'Movies';
+const seriesTag = 'Series';
+const humorTag = 'Humor';
+const wisdomTag = 'Wisdom';
+const marvelComicsTag = 'Marvel Comics';
+const sciFiTag = 'SciFi';
+
+
+/***
  * `quotes` array
 ***/
 const quotes = [];
 quotes.push({
     quote: 'Strength does not come from physical capacity. It comes from an indomitable will.',
-    source: 'Mahatma Gandhi'
+    source: 'Mahatma Gandhi',
+    tags: [wisdomTag]
 });
 quotes.push({
     quote: 'Your vision will become clear only when you look into your heart ... Who looks outside, dreams. Who looks inside, awakens.',
-    source: 'Carl Gustav Jung'
+    source: 'Carl Gustav Jung',
+    tags: [wisdomTag]
 });
 quotes.push({
     quote: 'Part of the journey is the end.',
     source: 'Tony Stark',
     citation: 'Avengers: Endgame',
-    year: '2019'
+    year: '2019',
+    tags: [moviesTag, marvelComicsTag]
 });
 quotes.push({
     quote: 'Everything That Has A Beginning Has An End.',
     source: 'The Oracle',
     citation: 'The Matrix: Revolutions',
-    year: '2003'
+    year: '2003',
+    tags: [moviesTag, sciFiTag]
 });
 quotes.push({
     quote: 'Denial is the most predictable of all human responses.',
     source: 'The Architect',
     citation: 'The Matrix: Reloaded',
-    year: '2003'
+    year: '2003',
+    tags: [moviesTag, sciFiTag]
 });
 quotes.push({
     quote: 'Look at these poor fuckers. They don\'t even know what\'s about to hit \'em. The hammer of God.',
     source: 'Erlich Bachman',
     citation: 'Silicon Valley',
-    year: '2014'
+    year: '2014',
+    tags: [seriesTag, humorTag]
 });
 
 /***
@@ -76,6 +93,9 @@ function printQuote() {
     //Check for source property
     if (quote.hasOwnProperty('year')) {
         htmlOutput += `<span class="year"> ${quote.year} </span>`;
+    }
+    if (quote.hasOwnProperty('tags')) {
+        htmlOutput += `<span class="tags"> Tags: ${quote.tags.join(', ')} </span>`;
     }
     // Close the paragraph
     htmlOutput += '</p>';
